53a858f033643899a72fd40ef046db319b5b70b7
jmdgab75 <gabrieljmd75@gmail.com>
2023-11-21 19:36:00 -0400

    commit final

--- js/main.js
+++ js/main.js
@@ -1,52 +1,48 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', async () => {
-    try {
-      let reg;
-      reg = await navigator.serviceWorker.register('/sw.js', { type: "module" });
-      console.log('Service worker registrada!', reg);
-    } catch (err) {
-      console.log('Service worker registro falhou', err);
-    }
+      try {
+          let reg;
+          reg = await navigator.serviceWorker.register('/sw.js', { type: "module" });
+
+          console.log('Service worker registrada! 😎', reg);
+      } catch (err) {
+          console.log('😥 Service worker registro falhou: ', err);
+      }
   });
 }
 
-let posicaoInicial;
-const capturarLocalizacao = document.getElementById( 'localizacao');
-const latitude = document.getElementById( 'latitude' );
-const longitude = document.getElementById( 'longitude' );
- 
-let gmap_canvas = document.getElementById ("gmap_canvas");
+let posicaoInicial;//variavel para capturar a posicao
+const capturarLocalizacao = document.getElementById('localizacao');
+const latitude = document.getElementById('latitude');
+const longitude = document.getElementById('longitude');
+const map = document.getElementById('mapa');
 
-const sucesso = (posicao) => {
-    posicaoInicial = posicao;
-    latitude.innerHTML = posicaoInicial.coords.latitude;
-    longitude.innerHTML = posicaoInicial.coords.longitude;
-    map.src = "https://maps.google.com/maps?q" + 
-
-
-    const gmap_canvas.src = "https://maps.google.com/maps?q=636+5th+Ave%2C+New+York&t=&z=13&ie=UTF8&iwloc=&output=embed";
-    
-}
-
-const erro = (error) => {
-    let errorMessage;
-    switch(error.code){
-    case 0:
-         errorMessage = "Erro desconhecido"
-    break;
-    case 1:
-          errorMessage = "Permissão negada!"
-    break;
-    case 2:
-          errorMessage = "Captura de posição indisponível!"
-    break;
-    case 3:
-          errorMessage = "Tempo de excedido!"
-    break;
-  }
-    console.log( 'Ocorreu um erro:' + errorMessage);
+const sucesso = (posicao) => {//callback de sucesso para captura da posicao
+  posicaoInicial = posicao;
+  latitude.innerHTML = posicaoInicial.coords.latitude;
+  longitude.innerHTML = posicaoInicial.coords.longitude;
+  map.src = "http://maps.google.com/maps?q=" + posicaoInicial.coords.latitude + "," + posicaoInicial.coords.longitude + "&z=16&output=embed";
 };
 
-capturarLocalizacao.addEventListener('click',() => {
-    navigator.geolocation.getCurrentPosition(sucesso, erro);
+const erro = (error) => {//callback de error (falha para captura de localizacao)
+  let errorMessage;
+  switch (error.code) {
+      case 0:
+          errorMessage = "Erro desconhecido"
+          break;
+      case 1:
+          errorMessage = "Permissão negada!"
+          break;
+      case 2:
+          errorMessage = "Captura de posição indisponível!"
+          break;
+      case 3:
+          errorMessage = "Tempo de solicitação excedido!"
+          break;
+  }
+  console.log('Ocorreu um erro: ' + errorMessage);
+};
+
+capturarLocalizacao.addEventListener('click', () => {
+  navigator.geolocation.getCurrentPosition(sucesso, erro);
 });
